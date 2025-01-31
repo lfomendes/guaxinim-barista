845befe2294cc258de1e1411175b2b354e90c0af
Paulo Reis <prsc.reis@gmail.com>
2025-01-31 13:45:32 -0500

    Setting environment variable overriding

--- guaxinim/core/guaxinim_bot.py
+++ guaxinim/core/guaxinim_bot.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 from guaxinim.core.coffee_data import CoffeePreparationData
 
 # Load environment variables
-load_dotenv()
+load_dotenv(override=True)
 
 
 class GuaxinimBot:
